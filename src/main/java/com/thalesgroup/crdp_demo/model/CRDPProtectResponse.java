92a32cdb71da512fb549ea71d4978def706ff920
anugram <anurag.jain@thalesgroup.com>
2025-01-15 12:47:04 -0500

    first commit

---
+++ src/main/java/com/thalesgroup/crdp_demo/model/CRDPProtectResponse.java
@@ -0,0 +1,16 @@
+package com.thalesgroup.crdp_demo.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CRDPProtectResponse {
+    @JsonProperty("protected_data")
+	private String cipherText;
+
+	public String getCipherText() {
+		return cipherText;
+	}
+
+	public void setCipherText(String cipherText) {
+		this.cipherText = cipherText;
+	}
+}
---
+++ src/main/java/com/thalesgroup/crdp_demo/model/CRDPRevealResponse.java
@@ -0,0 +1,19 @@
+package com.thalesgroup.crdp_demo.model;
+
+public class CRDPRevealResponse {
+    private String data;
+    private String message;
+
+    public String getData() {
+        return data;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
